cc75a6085b410687d4fe0ada33d56c870836b5c4
Nimul99 <htnimul99@gmail.com>
2023-07-24 13:15:27 +0600

    ping mongodb

---
+++ index.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.gflxnku.mongodb.net/?retryWrites=true&w=majority`;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // await client.connect();
+  
+      // ----------------------------------COLLECTION----------------------------------
+      const usersCollection = client.db("ClgBooking").collection("users");
+
+      const classCollection = client.db("ClgBooking").collection("Courses");
+      const myClassCollection = client.db("ClgBooking").collection("myclass");
+      const bookMarkCollection = client.db("ClgBooking").collection("bookMarks");
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+
+  run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("LearnUp is sitting");
+});
+
+app.listen(port, () => {
+  console.log(`LearnUp is sitting on port ${port}`);
+});
